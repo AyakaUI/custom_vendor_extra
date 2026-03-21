8f30f609fb9c1b5414f174399ae16408599f517b
HDzungx <hdzungx@gmail.com>
2026-03-22 02:17:56 +0700

    extra: Import SystemUIClocks
    Signed-off-by: HDzungx <hdzungx@gmail.com>

---
+++ product.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2026 HertzifyOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Clocks (SystemUI)
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap
